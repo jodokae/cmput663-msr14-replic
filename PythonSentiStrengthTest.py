d4f561d925b869c8a66b3d63b7bb3aa22e181fd7
Moein Owhadi Kareshk <moein.owhadi@gmail.com>
2017-09-30 00:43:03 -0600

    Add data to database

--- PythonSentiStrengthTest.py
+++ PythonSentiStrengthTest.py
@@ -1,5 +1,9 @@
 import subprocess
 import shlex
+import os
+import numpy as np
+import copy
+
 def RateSentiment(sentiString):
     #open a subprocess using shlex to get the command line string into the correct args list format
     #Modify the location of SentiStrength.jar and D:/SentiStrength_Data/ if necessary
@@ -11,10 +15,39 @@ def RateSentiment(sentiString):
     #convert from byte
     stdout_text = stdout_byte.decode("utf-8") 
     #replace the tab with a space between the positive and negative ratings. e.g. 1    -5 -> 1 -5
-    stdout_text = stdout_text.rstrip().replace(""," ")
-    return stdout_text + " " + sentiString
+    stdout_text = stdout_text.rstrip().replace("\t"," ")
+    return stdout_text 
 #An example to illustrate calling the process.
-print(RateSentiment('It was not good.'))
+##################################################################################print(RateSentiment('I am bad'))
 #The above is OK for one text but inefficient to repeatedly call for many texts. Try instead: 
 #  either modify the above to submit a file
 #  or modify the above to send multiple lines through multiple calls of p.communicate(b)
+
+
+import pymysql
+conn = pymysql.connect(host='localhost', user='msr14', passwd='msr14', db='msr14')
+cur = conn.cursor()
+cur.execute('select id from commit_comments;')
+idList = copy.copy(cur)
+for commitID in idList:
+	if True:
+		id = commitID[0]
+		cur.execute('select body from commit_comments where id = ' + str(id) + ';')
+		bodyList = copy.copy(cur)
+
+		body = [b[0].replace('\n', '').replace('\r', '') for b in bodyList][0]
+		sentiment = RateSentiment(body)
+		a, b = sentiment.split()
+		c  = [int(a), int(b)]
+		r = 2
+		if np.abs(c[0]) > np.abs(c[1]):
+			r = 0
+		else:
+			r = 1
+		s = c[r]
+		print(id)
+		cur.execute('UPDATE commit_comments SET sentiment = ' + str(s) + ' WHERE id =  ' + str(id) + '')	
+
+cur.close()
+conn.commit()
+conn.close()
---
+++ cmput663-msr14-replic-master/README.md
@@ -0,0 +1,54 @@
+# cmput663-msr14-replic
+
+# Dataset # 
+
+Download mysql dataset http://ghtorrent.org/msr14.html
+
+``` 
+$ mysql -u root -p
+mysql > create user 'msr14'@'localhost' identified by 'msr14';
+mysql> create database msr14;
+mysql> GRANT ALL PRIVILEGES ON msr14.* to msr14@'localhost';
+mysql> flush privileges;
+# Exit MySQL prompt
+# PW: msr14
+
+$ zcat msr14-mysql.gz |mysql -u msr14 -p msr14
+$ mysql -u msr14 -p msr14
+
+#TRY: 
+mysql> select language,count(*) from projects where forked_from is null group by language;
+
+#Example: My result was not exactly the same
++------------+----------+
+| language   | count(*) |
++------------+----------+
+| C          |       10 |
+| C#         |        8 |
+| C++        |        8 |
+| CSS        |        3 |
+| Go         |        1 |
+| Java       |        8 |
+| JavaScript |        9 |
+| PHP        |        9 |
+| Python     |       10 |
+| R          |        4 |
+| Ruby       |       10 |
+| Scala      |        9 |
+| TypeScript |        1 |
++------------+----------+
+13 rows in set (0.01 sec)
+```
+
+http://ghtorrent.org/files/schema.pdf
+
+# Senti Strength #
+
+Get SentiStrength for Java (http://sentistrength.wlv.ac.uk/) For academic purpose for free if you write a mail to m (dot) thelwall (at) wlv.ac.uk
+
+Test: 
+```
+$ java -jar SentiStrengthCom.jar sentidata SentiStrengthData/ input testfile.txt
+```
+
+Output gets generated as txt
