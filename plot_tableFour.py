aafa5f3594f34fded1d32f77dc1ae302dfba3b87
Johannes <sir_stormrider@web.de>
2017-10-02 16:10:57 -0600

    pretty tables

--- plot_tableFour.py
+++ plot_tableFour.py
@@ -1,6 +1,7 @@
 import pymysql
 import numpy
 import matplotlib.pyplot as plt
+from prettytable import PrettyTable
 
 table_four_query = 'select daytime,count(distinct commit_comments.id),avg(commit_sentiment.sentiment),std(commit_sentiment.sentiment) \
 from commit_comments \
@@ -11,7 +12,11 @@ conn = pymysql.connect(host='localhost', user='msr14', passwd='msr14', db='msr14
 curs = conn.cursor() #Use a client side cursor so you can access curs.rowcount
 numrows = curs.execute(table_four_query)
 
+times = ['Morning', 'Afternoon', 'Evening', 'Night']
+
+t = PrettyTable(['Time of Day', 'Commits', 'Mean', 'Stand. Dev.'])
 A = [row for row in curs]
 for item in A:
-    print(item)
-
+    t.add_row([times[item[0]], item[1], item[2], item[3]])
+    
+print(t)
--- plot_tableThree.py
+++ plot_tableThree.py
@@ -1,48 +1,22 @@
 import pymysql
 import numpy
 import matplotlib.pyplot as plt
+from prettytable import PrettyTable
 
-figure_one_query = 'select projects.name, AVG(commit_sentiment.sentiment) \
-FROM commit_comments \
-INNER JOIN commits ON commits.id = commit_comments.commit_id \
-INNER JOIN projects on projects.id = commits.project_id \
-INNER JOIN commit_sentiment on commit_sentiment.id = commit_comments.id \
-WHERE projects.name LIKE \'Jquery\' OR projects.name LIKE \'Rails\' OR projects.name LIKE \'CraftBukkit\' OR \
-projects.name LIKE \'Diaspora\' OR projects.name LIKE \'MaNGOS\' OR projects.name LIKE \'TrinityCore\' \
-GROUP BY projects.name;'
-
-figure_two_query = 'select projects.name, commit_sentiment.sentiment_type, COUNT(commit_sentiment.sentiment_type) \
-FROM commit_comments \
-INNER JOIN commits ON commits.id = commit_comments.commit_id \
-INNER JOIN projects on projects.id = commits.project_id \
-INNER JOIN commit_sentiment on commit_sentiment.id = commit_comments.id \
-WHERE projects.name LIKE \'Jquery\' OR projects.name LIKE \'Rails\' OR projects.name LIKE \'CraftBukkit\' OR \
-projects.name LIKE \'Diaspora\' OR projects.name LIKE \'MaNGOS\' OR projects.name LIKE \'TrinityCore\' \
-GROUP BY projects.name, commit_sentiment.sentiment_type;'
-
-table_two_query = 'SELECT projects.language, COUNT( commit_comments.id), AVG(commit_sentiment.sentiment), STD(commit_sentiment.sentiment) \
-FROM commit_comments \
-INNER JOIN commits on commit_comments.commit_id = commits.id \
-INNER JOIN projects on commits.project_id = projects.id \
-INNER JOIN commit_sentiment on commit_sentiment.id = commit_comments.id \
-where projects.language like \'C\' or projects.language like \'C++\' or projects.language like \'Java\' or projects.language like \'Python\' or projects.language like \'Ruby\' \
-GROUP BY projects.language'
-
-table_three_query = 'select dayname(commit_comments.created_at),count(distinct commit_comments.id),avg(commit_sentiment.sentiment),std(commit_sentiment.sentiment) \
+table_three_query = 'select weekday(commit_comments.created_at),count(distinct commit_comments.id),avg(commit_sentiment.sentiment),std(commit_sentiment.sentiment) \
 from commit_comments \
 inner join commit_sentiment on commit_comments.id = commit_sentiment.id \
-group by dayname(created_at);'
-
-table_four_query = 'select daytime,count(distinct commit_comments.id),avg(commit_sentiment.sentiment),std(commit_sentiment.sentiment) \
-from commit_comments \
-inner join commit_sentiment on commit_comments.id = commit_sentiment.id \
-group by daytime'
+group by weekday(created_at);'
 
 conn = pymysql.connect(host='localhost', user='msr14', passwd='msr14', db='msr14')
 curs = conn.cursor() #Use a client side cursor so you can access curs.rowcount
 numrows = curs.execute(table_three_query)
 
+weekday = ['Monday', 'Tuesday', 'Wednessday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+t = PrettyTable(['Weekday', 'Commits', 'Mean', 'Stand. Dev.'])
 A = [row for row in curs]
 for item in A:
-    print(item)
-
+    t.add_row([weekday[item[0]], item[1], item[2], item[3]])
+    
+print(t)
